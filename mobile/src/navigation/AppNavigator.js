ff86b161dff17b5cb6a28d6159622c51f70db32a
juangoji13 <juangoji13@gmail.com>
2026-03-12 20:27:13 -0500

    Fix syntax error in AppNavigator.js

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -84,6 +84,7 @@ export default function AppNavigator() {
 
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {!user ? (
                 <>
                     <Stack.Screen name="Login" component={LoginScreen} />
                     <Stack.Screen name="Register" component={RegisterScreen} />
